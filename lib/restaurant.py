006f7331a04064f5f412dd1be425760b4b2e5d2d
philipwanjiku <muhorophilip7@gmail.com>
2024-01-08 08:51:40 +0300

    restaurant

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -31,6 +31,15 @@ class Customer(Base):
             if review.restaurant == restaurant:
                 session.delete(review)
         session.commit()
-    
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+
+    reviews = relationship('Review', back_populates='restaurant')
+
 
     
